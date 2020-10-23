af186b7cb1d08e279d71c7d00b2a3f608a3e422c
Nguyen Hong Vu <vunh76@gmail.com>
2020-10-23 07:58:37 +0700

    disable schedule for dag

--- tutorial.py
+++ tutorial.py
@@ -35,7 +35,7 @@ dag = DAG(
     'tutorial',
     default_args=default_args,
     description='A simple tutorial DAG',
-    schedule_interval=timedelta(days=1),
+    schedule_interval=None,
 )
 
 # t1, t2 and t3 are examples of tasks created by instantiating operators
