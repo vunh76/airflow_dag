b7edbef70830c843ddd191a14b16b960b0639bc2
Vu Nguyen <vunh76@gmail.com>
2020-10-23 14:31:51 +0700

    Update k8s_init_pod.py

--- k8s_init_pod.py
+++ k8s_init_pod.py
@@ -22,36 +22,11 @@ dag = DAG(
 
 start = DummyOperator(task_id='run_this_first', dag=dag)
 
-volume = k8s.V1Volume(
-    name='git-volume',
-    empty_dir=k8s.V1EmptyDirVolumeSource(),
-)
-
-init_environments = [k8s.V1EnvVar(name='GIT_SYNC_REPO', value='https://github.com/vunh76/udacity_cs_101'), k8s.V1EnvVar(name='GIT_SYNC_BRANCH', value='master'), k8s.V1EnvVar(name='GIT_SYNC_ONE_TIME', value='true')]
-init_container_volume_mounts = [
-    k8s.V1VolumeMount(mount_path='/tmp/git', name='git-volume', sub_path=None, read_only=False)
-]
-init_container = k8s.V1Container(
-    name="init-container",
-    image="k8s.gcr.io/git-sync:v3.0.1",
-    volume_mounts=init_container_volume_mounts,
-    env=init_environments
-)
-
-volume_mount = k8s.V1VolumeMount(
-    name='git-volume', mount_path='/tmp/git', sub_path=None, read_only=True
-)
 passing = KubernetesPodOperator(namespace='airflow',
-                                image="python:3.7",
-                                cmds=["python", "/tmp/git/abacus.py"],
-                                labels={"task": "abacus"},
                                 name="abacus-test",
                                 task_id="abacus-task",
                                 get_logs=True,
-                                #is_delete_operator_pod=True,
-                                init_containers=[init_container],
-                                volumes=[volume],
-                                volume_mounts=[volume_mount],
+                                pod_template_file="git_init_container.yaml",
                                 dag=dag
                                 )
 
