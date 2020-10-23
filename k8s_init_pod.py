5fddd6598a45af65476d8b3d58a9198dc859cdd0
Vu Nguyen <vunh76@gmail.com>
2020-10-23 15:28:35 +0700

    Update k8s_init_pod.py

--- k8s_init_pod.py
+++ k8s_init_pod.py
@@ -14,7 +14,7 @@ default_args = {
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 1,
-    'retry_delay': timedelta(minutes=5)
+    'retry_delay': timedelta(minutes=1)
 }
 
 dag = DAG(
@@ -22,13 +22,58 @@ dag = DAG(
 
 start = DummyOperator(task_id='run_this_first', dag=dag)
 
-passing = KubernetesPodOperator(namespace='airflow',
-                                name="abacus-test",
-                                task_id="abacus-task",
-                                get_logs=True,
-                                full_pod_spec=None,
-                                pod_template_file="git_init_container.yaml",
-                                dag=dag
-                                )
+volume_mount = k8s.V1VolumeMount(
+    name='git-volume', mount_path='/tmp/git', sub_path=None, read_only=True
+)
 
-passing.set_upstream(start)
+volume = k8s.V1Volume(
+    name='git-volume',
+    empty_dir=k8s.V1EmptyDirVolumeSource()
+)
+
+init_container_volume_mounts = [
+    k8s.V1VolumeMount(mount_path='/tmp/git', name='git-volume', sub_path=None, read_only=False)
+]
+
+init_environments = [
+    k8s.V1EnvVar(name='GIT_SYNC_REPO', value='https://github.com/vunh76/udacity_cs_101.git'),
+    k8s.V1EnvVar(name='GIT_SYNC_BRANCH', value='master'),
+    k8s.V1EnvVar(name='GIT_SYNC_ONE_TIME', value='true'),
+    k8s.V1EnvVar(name='GIT_SYNC_DEST', value='cs101'),
+]
+
+init_container = k8s.V1Container(
+    name="git-sync",
+    image="k8s.gcr.io/git-sync:v3.0.1",
+    env=init_environments,
+    volume_mounts=init_container_volume_mounts
+)
+
+abacus = KubernetesPodOperator(
+        namespace='airflow',
+        image="python:3.7",
+        cmds=["python"],
+        arguments=["/tmp/git/cs101/abacus.py"],
+        labels={"cs101": "abacus"},
+        volumes=[volume],
+        volume_mounts=[volume_mount],
+        name="airflow-abacus-pod",
+        task_id="abacus-task",
+        is_delete_operator_pod=True,
+        init_containers=[init_container],
+        priority_class_name="medium",
+        dag=dag
+    )
+
+# passing = KubernetesPodOperator(namespace='airflow',
+#                                 name="abacus-test",
+#                                 task_id="abacus-task",
+#                                 get_logs=True,
+#                                 full_pod_spec=None,
+#                                 pod_template_file="git_init_container.yaml",
+#                                 dag=dag
+#                                 )
+
+
+
+abacus.set_upstream(start)
